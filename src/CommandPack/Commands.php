3a2b276b18b8efdf019788d7bf7e91f8ee002b1f
benzodoesdev <33678572+benzodoesdev@users.noreply.github.com>
2018-06-20 05:12:37 -0500

    Update Commands.php

--- src/CommandPack/Commands.php
+++ src/CommandPack/Commands.php
@@ -4,8 +4,7 @@ namespace CommandPack;
 
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;use pocketmine\event\Listener;
 use pocketmine\Player;
 
 use pocketmine\command\Command;
@@ -18,13 +17,13 @@ class Commands extends PluginBase implements Listener{
 
         public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
 
-
                 switch($cmd->getName()) {
                 case "feed":
                     if($sender instanceof Player){
-                        if($sender->hasPermission)("use.feed")){    
-                        $sender->setFood(20);
-                        $sender->sendMessage("You are no longer hungry!");                        }    
+                        if($sender->hasPermission)("use.feed")){                            
+                           $sender->setFood(20);
+                        $sender->sendMessage("&eYou are no longer hungry!");                        
+                        }    
                     }
                 return true;
                 break;
@@ -33,17 +32,17 @@ class Commands extends PluginBase implements Listener{
                     if($sender instanceof Player){
                         if($sender->hasPermission("use.nick")){
                         $sender->setNameTag($args[0]);
-                        $sender->sendMessage("Nick has been set succesfully");
-                        }"
+                        $sender->sendMessage("&eNick has been set succesfully");
+                        }
                     }
                 return true;
                 break;
                             
                 case "heal":
                     if($sender instanceof Player){
-                        if($sender-hasPermission("use.heal")){
+                        if($sender->hasPermission("use.heal")){
                         $sender->setHealth(20);
-                        $sender->sendMessage("You have been healed!");
+                        $sender->sendMessage("&You have been healed!");
                         }
                     }
                 return true;
@@ -54,7 +53,7 @@ class Commands extends PluginBase implements Listener{
                         if($sender->hasPermission("use.cure")){
                         $sender->setHealth(20);
                         $sender->setFood(20);
-                        $sender->sendMessage("You have been cured!");
+                        $sender->sendMessage("&eYou have been cured!");
                         }
                     }        
                 return true;
@@ -65,7 +64,7 @@ class Commands extends PluginBase implements Listener{
                         if($sender->hasPermission("use.clearinv")){
                         $sender->getInventory()->clearAll();
                         $sender->getArmorInventory()->clearAll();
-                        $sender->sendMessage("Inventory Has been cleared Succesfully");
+                        $sender->sendMessage("&eInventory Has been cleared Succesfully");
                         }    
                     }
                 return true;
@@ -76,6 +75,7 @@ class Commands extends PluginBase implements Listener{
                         if($sender->hasPermission("use.fly")){
                         if($sender->getAllowFlight()){
                             $sender->setAllowFlight(true);
+                            $sender->sendMessage("&eFlight has been enabled!")
                         } else {
                             $sender->setAllowFlight(false);
                         }
